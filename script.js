da00d7a9fa84715e33ca97f1a565d8dbd3fec90e
Arjun <arjun7.saili@gmail.com>
2021-08-17 14:47:34 -0400

    Altered organization to be more logical, Added turn switching

--- script.js
+++ script.js
@@ -7,41 +7,66 @@ const game = (()=> {
         const addMove = (index, playerSymbol) => {
             if (gameboardArray[index] === null){
                 gameboardArray.splice(index, 1, playerSymbol);
-                displayController.render();
+                displayController.render(); 
+                displayController.endTurn();
             }
         }
-
-
         return {addMove, gameboardArray}
-    })();
-
-    const displayController = (()=> {
-        const render = () => {
-            for(let i=1; i<=gameBoard.gameboardArray.length; i++){
-                gridboxes[i-1].textContent = gameBoard.gameboardArray[i-1];
-            }
-        }
-        return {render}
     })();
 
     const Player = ((playerNum) => {
         const symbol = playerNum == 1 ?  'X' : 'O';
         const getPlayerNum = () => playerNum;
         const getSymbol = () => symbol;
-        const play = () => {
-            for(let i = 0; i < gridboxes.length; i++){
-                gridboxes[i].addEventListener('click', makeMove.bind(null, i));
-            }
-        }
         const makeMove = (index) => {
             gameBoard.addMove(index, symbol);
         }
 
-        return {play, getSymbol, getPlayerNum}
+        return {makeMove, getSymbol, getPlayerNum}
     });
 
-    displayController.render();
-    const player1 = Player(2);
-    player1.play();
-    console.log(player1.getSymbol());
+    const displayController = (()=> {
+        const player1 = Player(1);
+        const player2 = Player(2);
+        const startGame = () => {
+            render();
+            bindEvents();
+        }
+
+        let currentTurn = player1;
+
+        const endTurn = () => {
+            if (currentTurn == player1){
+                currentTurn = player2;
+            }
+            else{
+                currentTurn = player1;
+            }
+        }
+
+        const render = () => {
+            for(let i=1; i<=gameBoard.gameboardArray.length; i++){
+                gridboxes[i-1].textContent = gameBoard.gameboardArray[i-1];
+            }
+        }
+
+        const bindEvents = () => {
+            for(let i = 0; i < gridboxes.length; i++){
+                gridboxes[i].addEventListener('click', playerTurn.bind(null, i));
+            }
+        }
+
+        const playerTurn = (i) => {
+            if (currentTurn == player1){
+                player1.makeMove(i);
+            }
+            else{
+                player2.makeMove(i);
+            }
+        }
+        return {startGame, endTurn, render}
+    })();
+
+    displayController.startGame();
+
 })();
