8958073a015d352644fcf16692bdab80118c1142
Arjun <arjun7.saili@gmail.com>
2021-08-17 14:07:23 -0400

    AddedAbility for Players to add to gameboard

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-const gameBoard = (() => {
-    const gameboardArray = ['X','O','X','X','O','X','O','O','X'];
+const game = (()=> {
+    const gridboxes = document.querySelectorAll('.gridbox')
+    
+    const gameBoard = (() => {
+        const gameboardArray = [null, null, null, null, null, null, null, null, null];
 
-    const createGameboard = () =>{
-        for(let i=1; i<=gameboardArray.length; i++){
-            document.querySelector('#box-' + i).textContent = gameboardArray[i-1];
+        const addMove = (index, playerSymbol) => {
+            if (gameboardArray[index] === null){
+                gameboardArray.splice(index, 1, playerSymbol);
+                displayController.render();
+            }
         }
-    }
-    return {gameboardArray, createGameboard}
+
+
+        return {addMove, gameboardArray}
+    })();
+
+    const displayController = (()=> {
+        const render = () => {
+            for(let i=1; i<=gameBoard.gameboardArray.length; i++){
+                gridboxes[i-1].textContent = gameBoard.gameboardArray[i-1];
+            }
+        }
+        return {render}
+    })();
+
+    const Player = ((playerNum) => {
+        const symbol = playerNum == 1 ?  'X' : 'O';
+        const getPlayerNum = () => playerNum;
+        const getSymbol = () => symbol;
+        const play = () => {
+            for(let i = 0; i < gridboxes.length; i++){
+                gridboxes[i].addEventListener('click', makeMove.bind(null, i));
+            }
+        }
+        const makeMove = (index) => {
+            gameBoard.addMove(index, symbol);
+        }
+
+        return {play, getSymbol, getPlayerNum}
+    });
+
+    displayController.render();
+    const player1 = Player(2);
+    player1.play();
+    console.log(player1.getSymbol());
 })();
-
-const displayController = (()=> {
-
-})();
-
-const Player = (() => {
-    // Add Properties and Methods here!
-});
-
-gameBoard.createGameboard();
